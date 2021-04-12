96a51416b8abc9381d7c816a97b184219ec48d61
oodumosu <oodumosu@ithaca.edu>
2021-04-12 16:04:57 -0400

    Created Stats Classes

---
+++ EnemyQuantStats.java
@@ -0,0 +1,36 @@
+public class EnemyQuantStats {
+    public double cr;
+    public int exp;
+
+    public EnemyQuantStats() {
+        this.cr = 0;
+        this.exp = 0;
+    }
+
+    /**
+     * 
+     * @param cr  - Combat rating, can range from 0-30
+     * @param exp - Eexperience points
+     */
+    public EnemyQuantStats(double cr, int exp) {
+        this.cr = cr;
+        this.exp = exp;
+    }
+
+    public double getCr() {
+        return this.cr;
+    }
+
+    public void setCr(double cr) {
+        this.cr = cr;
+    }
+
+    public int getexp() {
+        return this.exp;
+    }
+
+    public void setexp(int exp) {
+        this.exp = exp;
+    }
+
+}
