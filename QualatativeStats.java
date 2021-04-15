d9e2c802cf42c5559e494caf624764e1dcff7c75
oodumosu <oodumosu@ithaca.edu>
2021-04-15 09:43:43 -0400

    Added "gargantuan" size

--- QualatativeStats.java
+++ QualatativeStats.java
@@ -4,6 +4,7 @@ import java.util.*;
  * Includes a constructor to set inital stats
  * Includes public getters and setters for all stats to allow the DungeonMaster to edit stats
  **/
+
 public class QualatativeStats {        
     public enum alignment{
         unaligned,
@@ -22,7 +23,8 @@ public class QualatativeStats {
         small,
         medium,
         large,
-        huge;
+        huge,
+        gargantuan;
     }
     public ArrayList<String> languages;
     public alignment align;
