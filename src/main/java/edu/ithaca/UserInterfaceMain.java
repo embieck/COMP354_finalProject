e8b39b4ff429341b776387118db222a481f8619a
pseudaimonia <57679024+pseudaimonia@users.noreply.github.com>
2021-04-16 03:10:28 -0400

    start to writing character info to file

--- src/main/java/edu/ithaca/UserInterfaceMain.java
+++ src/main/java/edu/ithaca/UserInterfaceMain.java
@@ -36,10 +36,76 @@ public class UserInterfaceMain {
         File f = new File(filename);
         BufferedWriter bw = new BufferedWriter(new FileWriter(f));
         bw.write("party info to be added");
-        /*
+    
         for(int i=0;i<p.getPartySize();i++){
-            bw.write("info per party member to be added");
-        }*/
+            PartyMember c = p.getCharacter(i);
+            bw.write(c.getName());
+            //write class
+            if(c.getCharacterClass()==CharacterClass.BARD){
+                bw.write("bard");
+            }
+            else if(c.getCharacterClass()==CharacterClass.BARBARIAN){
+                bw.write("barbarian");
+            }
+            else if(c.getCharacterClass()==CharacterClass.FIGHTER){
+                bw.write("fighter");
+            }
+            else if(c.getCharacterClass()==CharacterClass.ROGUE){
+                bw.write("rogue");
+            }
+            else if(c.getCharacterClass()==CharacterClass.SORCERER){
+                bw.write("sorcerer");
+            }
+            else if(c.getCharacterClass()==CharacterClass.WARLOCK){
+                bw.write("warlock");
+            }
+            else if(c.getCharacterClass()==CharacterClass.WIZARD){
+                bw.write("wizard");
+            }
+            else if(c.getCharacterClass()==CharacterClass.CLERIC){
+                bw.write("cleric");
+            }
+            else if(c.getCharacterClass()==CharacterClass.MONK){
+                bw.write("monk");
+            }
+            else if(c.getCharacterClass()==CharacterClass.RANGER){
+                bw.write("ranger");
+            }
+            else if(c.getCharacterClass()==CharacterClass.PALADIN){
+                bw.write("paladin");
+            }
+            else{
+                bw.write("druid");
+            }
+            //write race
+            if(c.getCharacterRace()==CharacterRace.DRAGONBORNE){
+                bw.write("dragonborne");
+            }
+            else if(c.getCharacterRace()==CharacterRace.HALFELF){
+                bw.write("half-elf");
+            }
+            else if(c.getCharacterRace()==CharacterRace.HALFLING){
+                bw.write("halfling");
+            }
+            else if(c.getCharacterRace()==CharacterRace.HALFORC){
+                bw.write("half-orc");
+            }
+            else if(c.getCharacterRace()==CharacterRace.GNOME){
+                bw.write("gnome");
+            }
+            else if(c.getCharacterRace()==CharacterRace.DWARF){
+                bw.write("dwarf");
+            }
+            else if(c.getCharacterRace()==CharacterRace.ELF){
+                bw.write("elf");
+            }
+            else if(c.getCharacterRace()==CharacterRace.TIEFLING){
+                bw.write("tiefling");
+            }
+            else{
+                bw.write("human");
+            }
+        }
         bw.close();
 
     }
