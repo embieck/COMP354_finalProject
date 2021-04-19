b3b0237433c26fccec623c8a75fd229f0c6941b4
kelseygrant <kgrant5@ithaca.edu>
2021-04-19 00:39:24 -0400

    enemy and partymember linked to qual and quan stats

--- src/main/java/edu/ithaca/Enemy.java
+++ src/main/java/edu/ithaca/Enemy.java
@@ -2,6 +2,10 @@ package edu.ithaca;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+
+import edu.ithaca.QualatativeStats.alignment;
+import edu.ithaca.QualatativeStats.size;
+
 import java.util.*;
 
 public class Enemy {
@@ -9,6 +13,8 @@ public class Enemy {
     private boolean isMagicUser;
     private String terrain;
     private String movementType;
+    private QuantativeStats quanStats;
+    private QualatativeStats qualStats;
 
 
     public boolean getIsHumanoid(){
@@ -27,6 +33,56 @@ public class Enemy {
         return movementType;
     }
 
+    //qualitative stats
+    public alignment getAlignment(){
+        return qualStats.getAlign();
+    }
+
+    public size getSize(){
+        return qualStats.getSize();
+    }
+
+    public ArrayList<String> getLanguages(){
+        return qualStats.getLanguages();
+    }
+
+    //quantitative stats
+    public int getMoveSpeed(){
+        return quanStats.getMoveSpeed();
+    }
+
+    public int getArmorClass(){
+        return quanStats.getArmorClass();
+    }
+
+    public int getHP(){
+        return quanStats.getHp();
+    }
+
+    public int getCon(){
+        return quanStats.getCon();
+    }
+
+    public int getStr(){
+        return quanStats.getStr();
+    }
+
+    public int getWis(){
+        return quanStats.getWis();
+    }
+
+    public int getIntel(){
+        return quanStats.getIntel();
+    }
+
+    public int getDex(){
+        return quanStats.getDex();
+    }
+
+    public int getCha(){
+        return quanStats.getCha();
+    }
+
     /**
      * Constructor
      * @param isHumanoid - gives a clue to the default behavior of the creatures
@@ -34,11 +90,13 @@ public class Enemy {
      * @param terrain - list of languages the character can speak
      * @param movementType - the char used to seperate in the languages parameter
      */
-    // public Enemy(boolean isHumanoid, boolean isMagicUser, String terrain, String movementType){
-    //     isHumanoid = false;
-    //     isMagicUser = false;
-    //     String terrain = "";
-    //     String movementType = "";
-        
-        //}
+    public Enemy(boolean isHumanoid, boolean isMagicUser, String terrain, String movementType,
+    QuantativeStats qualStats, QuantativeStats quanStats){
+        this.isHumanoid = isHumanoid;
+        this.isMagicUser = isMagicUser;
+        this.terrain = terrain;
+        this.movementType = movementType;        
+        this.qualStats = qualStats;
+        this.quanStats = quanStats;
+        }
     }
--- src/main/java/edu/ithaca/PartyMember.java
+++ src/main/java/edu/ithaca/PartyMember.java
@@ -1,7 +1,12 @@
 package edu.ithaca;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+// import java.util.LinkedList;
+// import java.util.List;
+
+import edu.ithaca.QualatativeStats;
+import edu.ithaca.QuantativeStats;
+import edu.ithaca.QualatativeStats.alignment;
+import edu.ithaca.QualatativeStats.size;
 
 enum CharacterClass {BARBARIAN, BARD, CLERIC, DRUID, FIGHTER, MONK, PALADIN, RANGER, ROGUE, SORCERER, 
     WARLOCK, WIZARD}
@@ -11,19 +16,71 @@ enum CharacterRace {DRAGONBORNE, DWARF, ELF, GNOME, HALFELF, HALFLING, HALFORC, 
 public class PartyMember {
 
     private String name;
-    private CharacterClass CharacterClass;
-    private CharacterRace CharacterRace;
+    private CharacterClass characterClass;
+    private CharacterRace characterRace;
+    private QualatativeStats qualStats;
+    private QuantativeStats quanStats;
 
     public String getName(){
         return name;
     }
 
     public CharacterClass getCharacterClass(){
-        return CharacterClass;
+        return characterClass;
     }
 
     public CharacterRace getCharacterRace(){
-        return CharacterRace;
+        return characterRace;
+    }
+
+    //qualitative stats
+    public alignment getAlignment(){
+        return qualStats.getAlign();
+    }
+
+    public size getSize(){
+        return qualStats.getSize();
+    }
+
+    public ArrayList<String> getLanguages(){
+        return qualStats.getLanguages();
+    }
+
+    //quantitative stats
+    public int getMoveSpeed(){
+        return quanStats.getMoveSpeed();
+    }
+
+    public int getArmorClass(){
+        return quanStats.getArmorClass();
+    }
+
+    public int getHP(){
+        return quanStats.getHp();
+    }
+
+    public int getCon(){
+        return quanStats.getCon();
+    }
+
+    public int getStr(){
+        return quanStats.getStr();
+    }
+
+    public int getWis(){
+        return quanStats.getWis();
+    }
+
+    public int getIntel(){
+        return quanStats.getIntel();
+    }
+
+    public int getDex(){
+        return quanStats.getDex();
+    }
+
+    public int getCha(){
+        return quanStats.getCha();
     }
 
     /**
@@ -32,12 +89,15 @@ public class PartyMember {
      * @param CharacterClass - enum that indicates class of the character
      * @param CharacterRace - enum that indicates race of the character
      */
-    // public PartyMember(String name, CharacterClass characterClass, CharacterRace characterRace){
-    //     this.name = "";
-    //     this.characterClass = characterClass;
-    //     this.characterRace = characterRace;
+    public PartyMember(String name, CharacterClass characterClass, CharacterRace characterRace,
+    QualatativeStats qualStats, QuantativeStats quanStats){
+        this.name = "";
+        this.characterClass = characterClass;
+        this.characterRace = characterRace;
+        this.qualStats = qualStats;
+        this.quanStats = quanStats;
         
-    // }
+    }
 
 }
 
